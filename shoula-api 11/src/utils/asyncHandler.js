3b974916d8d4205d4999292768450e0c510001b8
hasan19979 <hasan19979@gmail.com>
2026-09-17 16:45:54 +0300

    Delete shoula-api 11 directory

--- shoula-api 11/src/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-// بيلف أي route async حتى لو صار خطأ جواه، ينمرر تلقائياً للـ error handler
-// بدل ما يعلّق السيرفر أو يحتاج try/catch مكرر بكل مكان
-function asyncHandler(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
-
-module.exports = asyncHandler;
